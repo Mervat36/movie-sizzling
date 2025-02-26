90495a601db0e0eee04c2ffcbecb769fa55411cd
Mervat Habib <mervat2105283@miuegypt.edu.eg>
2025-02-26 13:17:04 +0200

    Config

---
+++ models/Video.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const VideoSchema = new mongoose.Schema({
+    title: String,
+    url: String,
+    description: String,
+}, { timestamps: true });
+
+module.exports = mongoose.model("Video", VideoSchema);
