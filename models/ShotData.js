91c5818ef70420c22fa4b987d8cac0f9b7fa4ed9
Ahmed21gamal <125803560+Ahmed21gamal@users.noreply.github.com>
2025-04-23 16:52:13 +0200

    shot model

---
+++ models/ShotData.js
@@ -0,0 +1,26 @@
+// models/ShotMetadata.js
+const mongoose = require("mongoose");
+
+const shotSchema = new mongoose.Schema({
+  movieTitle: String,
+  videoPath: String,
+  fps: Number,
+  shots: [
+    {
+      shotNumber: Number,
+      startFrame: Number,
+      middleFrame: Number,
+      endFrame: Number,
+      startTime: String,
+      middleTime: String,
+      endTime: String,
+      images: {
+        start: String,
+        middle: String,
+        end: String
+      }
+    }
+  ]
+});
+
+module.exports = mongoose.model("ShotMetadata", shotSchema);
